abfdba56f228f2998df473a5fdc52d74652e1409
Sami Olmari <sami@olmari.fi>
2018-05-30 11:49:18 +0300

    Working asteroid
    Sinnce most development was made within asteroid rpi itself
    cowboy style, there is just bunch of code to upload at this stage.
    
    Signed-off-by: Sami Olmari <sami@olmari.fi>

--- rpi/asteroid.py
+++ rpi/asteroid.py
@@ -18,18 +18,9 @@ log = logging.getLogger("asteroid")
 
 # Setup GPIO output pins, GPIO.BOARD
 main_light = 11
-aux_light1 = 12
-aux_light2 = 36
-
-## Old pins from gatekeeper, for reference
-# lights = 38
-# out3 = 32
-# out4 = 40
-# latch = 29
-# lightstatus = 31
-# in3 = 33
-# in4 = 35
-# in5 = 37
+relay2 = 13
+aux_light = 37
+piezo_element = 36
 
 class Pin:
     # Init (activate pin)
@@ -40,10 +31,12 @@ class Pin:
         # Set up GPIO output channels
         GPIO.setup(main_light, GPIO.OUT, initial=GPIO.HIGH)
         log.debug("initialized main_light, pin to high")
-        GPIO.setup(aux_light1, GPIO.OUT, initial=GPIO.LOW)
-        log.debug("initialized aux_light1, pin to low")
-        GPIO.setup(aux_light2, GPIO.OUT, initial=GPIO.HIGH)
-        log.debug("initialized aux_light2, pin to high")
+        GPIO.setup(relay2, GPIO.OUT, initial=GPIO.HIGH)
+        log.debug("initialized relay2, pin to high")
+        GPIO.setup(aux_light, GPIO.OUT, initial=GPIO.LOW)
+        log.debug("initialized aux_light, pin to low")
+        GPIO.setup(piezo_element, GPIO.OUT, initial=GPIO.LOW)
+        log.debug("initialized piezo_element, pin to low")
 
     def main_light_on(self):
         GPIO.output(main_light, GPIO.LOW)
@@ -51,17 +44,17 @@ class Pin:
     def main_light_off(self):
         GPIO.output(main_light, GPIO.HIGH)
 
-    def aux_light1_on(self):
-        GPIO.output(aux_light1, GPIO.HIGH)
+    def relay2_on(self):
+        GPIO.output(relay2, GPIO.LOW)
 
-    def aux_light1_off(self):
-        GPIO.output(aux_light1, GPIO.LOW)
+    def relay2_off(self):
+        GPIO.output(relay2, GPIO.HIGH)
 
-    def aux_light2_on(self):
-        GPIO.output(aux_light2, GPIO.LOW)
+    def aux_light_on(self):
+        GPIO.output(aux_light, GPIO.HIGH)
 
-    def aux_light2_off(self):
-        GPIO.output(aux_light2, GPIO.HIGH)
+    def aux_light_off(self):
+        GPIO.output(aux_light, GPIO.LOW)
 
 class Button(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -79,22 +72,22 @@ class Button(BaseHTTPRequestHandler):
         log.info("Remote button triggered")
         log.debug("Setting threads")
         main_light = threading.Thread(target=asteroid.run_main_light, args=())
-        aux_light1 = threading.Thread(target=asteroid.run_aux_light1, args=())
-        aux_light2 = threading.Thread(target=asteroid.run_aux_light2, args=())
+        aux_light = threading.Thread(target=asteroid.run_aux_light, args=())
+        piezo_element = threading.Thread(target=asteroid.run_piezo_element, args=())
         log.debug("Running threads")
         main_light.start()
-        aux_light1.start()
-        aux_light2.start()
+        aux_light.start()
+        piezo_element.start()
         log.debug("Threads started, waiting them to finish")
         main_light.join()
-        aux_light1.join()
-        aux_light2.join()
+        aux_light.join()
+        piezo_element.join()
         log.debug("All threads finished")
         return
 
 class Asteroid:
     def __init__(self):
-        self.runtime = 5
+        self.runtime = 10
         server_address = ("0.0.0.0", 8080)
         self.pin = Pin()
         self.httpd = HTTPServer(server_address, Button)
@@ -114,24 +107,35 @@ class Asteroid:
         self.pin.main_light_off()
         return
 
-    def run_aux_light1(self):
-        log.debug("Aux light 1 on")
+    def run_aux_light(self):
+        log.debug("Aux light on")
         timeout = time.time() + self.runtime
         while time.time() < timeout:
-            self.pin.aux_light1_on()
+            self.pin.aux_light_on()
             time.sleep(0.05)
-            self.pin.aux_light1_off()
+            self.pin.aux_light_off()
             time.sleep(0.05)
-        log.debug("Aux light 1 off")
+        log.debug("Aux light off")
         return
 
-    def run_aux_light2(self):
-        log.debug("Aux light 2 on")
-        self.pin.aux_light2_on()
-        time.sleep(self.runtime)
-        log.debug("Aux light 2 off")
-        self.pin.aux_light2_off()
-        return
+    def run_piezo_element(self):
+        log.debug("Playing audio")
+        start_time = time.time()
+        timeout = time.time() + self.runtime
+        max_frequency = 6000
+        min_frequency = 1000
+        frequency_step = 10
+        repetitions = 5
+        wait_time = float(self.runtime) / ((max_frequency - min_frequency) / frequency_step) / repetitions
+        Buzz = GPIO.PWM(piezo_element, 440)
+        Buzz.start(50)
+        while time.time() < timeout:
+            for i in range(max_frequency, min_frequency, -frequency_step):
+                #print(i)
+                Buzz.ChangeFrequency(i)
+                time.sleep(wait_time)
+        Buzz.stop()
+        GPIO.output(piezo_element, GPIO.LOW)
 
     def start(self):
         log.info("Starting asteroid")
